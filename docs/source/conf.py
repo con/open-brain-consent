d91079c41331d4e35d2ded7de25cf4b0fd4ca3da
Yaroslav Halchenko <debian@onerussian.com>
2015-06-13 11:03:51 -1000

    Added Chris to the list of copyright fellas

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open Brain Consent'
-copyright = u'2015, Yaroslav Halchenko'
+copyright = u'2015, Yaroslav Halchenko, Chris Filo Gorgolewski'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
